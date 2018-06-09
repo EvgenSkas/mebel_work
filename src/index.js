bfe10d17f3ecdef4d024484b9de6cf05e7cd4e26
Evgen Skaskevich <skaskevich23@gmail.com>
2018-06-09 14:08:11 +0300

    init_commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles.css'
+
+ReactDOM.render(
+	<h1>Hello World</h1>,
+	document.getElementById('root')
+)
+module.hot.accept();
