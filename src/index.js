7a52a19f03a124dbacfb07916c1422f6d0d2f809
Evgen Skaskevich <skaskevich23@gmail.com>
2018-06-10 22:24:48 +0300

    render_furn_page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
+import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from 'reducers';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux'
+import { Router, Route } from 'react-router';
+import Layout from 'containers/layout';
+// import Chairs from 'containers/chairs';
+import Main from 'containers/main';
+import Furniture from 'containers/furniture';
 
-import './styles.css'
 
+const store = createStore( reducers, composeWithDevTools(
+	applyMiddleware(thunk)
+));
+
+const history = syncHistoryWithStore( browserHistory, store)
+console.log(store.getState())
 ReactDOM.render(
-	<h1>Hello World</h1>,
+	<Provider store={store}>
+		<Router history={history}>
+			<Route component={Layout}>
+				<Route path="/" component={Main} />
+				<Route path="/furniture" component={Furniture} />
+
+			</Route>
+			{/* <Route path="phones/:id" component={Phone} />
+			<Route path="/basket" component={Basket} /> */}
+		</Router>
+	</Provider>,
 	document.getElementById('root')
 )
-module.hot.accept();
